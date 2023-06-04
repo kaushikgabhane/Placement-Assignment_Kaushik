bcd03364f16b1a03f2d71c7879ceb6c898c9f519
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-06-04 18:34:52 +0530

    context API dashboard project

---
+++ React/context-api/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header font-bold text-lg">
+       hello
+      </header>
+    </div>
+  );
+}
+
+export default App;
